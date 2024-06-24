d82fa027ad92be52f5a03bef8e238bb7b1fa214f
Akalyanandhakumar <167529056+Akalyanandhakumar@users.noreply.github.com>
2024-06-24 16:08:13 +0530

    linkedlist

---
+++ linkedlist.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+public class linkedlist{
+public static void main(String[] args) {
+    LinkedList<String> list= new LinkedList<>();
+    list.add("Akalya");
+    
+    System.out.print(list);
+}
+}
