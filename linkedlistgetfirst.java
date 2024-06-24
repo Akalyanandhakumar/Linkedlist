379e25f2b1e19d2e329e51d5fbc00acc3c3ebc70
Akalyanandhakumar <167529056+Akalyanandhakumar@users.noreply.github.com>
2024-06-24 22:43:38 +0530

    linkedlist.getFirst

---
+++ linkedlistgetfirst.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class linkedlistgetfirst {
+    public static void main(String[]args){
+    LinkedList<String>colors=new LinkedList<>();
+        colors.add("blue");
+        colors.add("red");
+        colors.add("green");
+        System.out.println(colors.getFirst());
+
+    }
+    
+}
