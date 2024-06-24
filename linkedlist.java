388069a38db2e476b38d863d6751a0227a647b37
Akalyanandhakumar <167529056+Akalyanandhakumar@users.noreply.github.com>
2024-06-24 16:12:10 +0530

    linkedlist.java

--- linkedlist.java
+++ linkedlist.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 public class linkedlist{
 public static void main(String[] args) {
     LinkedList<String> list= new LinkedList<>();
-    list.add("Akalya");
+    list.add(e :"Akalya");
     
     System.out.print(list);
 }
