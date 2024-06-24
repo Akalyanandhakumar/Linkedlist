4e5f805c6fa6e0f48f143a399c0356987f2c7c53
Akalyanandhakumar <167529056+Akalyanandhakumar@users.noreply.github.com>
2024-06-24 22:27:47 +0530

    linkedlist.add

---
+++ linkedlistadd.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class linkedlistadd{
+
+
+    public static void main(String[] args){
+        LinkedList<String> names=new LinkedList<>();
+          names.add("mukunth");
+          names.add("vinitha");
+          names.add("kavitha");
+          names.add("janu");
+          names.add("banu");
+          System.out.println(names);
+          LinkedList<String> name=new LinkedList<>();
+          name.add("abc");
+          name.add("efg");
+          System.out.println(name);
+          names.addAll(name);
+          System.out.print(names);
+    }
+}
+
