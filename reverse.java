d797e12401b6a479d5dcb1c5277eb136f579d2ed
Akalyanandhakumar <167529056+Akalyanandhakumar@users.noreply.github.com>
2024-06-25 18:05:05 +0530

    reverse.java

---
+++ reverse.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class reverse {
+    public static void main(String[] args){
+         LinkedList<String>number=new LinkedList<>();
+         number.add("akal");
+         number.add("alya");
+         number.add("tintu");
+         number.add("bintu");
+         System.out.println(number.reversed());
+    }
+}
