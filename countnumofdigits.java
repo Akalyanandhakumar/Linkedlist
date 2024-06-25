b011d59eb679321b3034498d4cbe68f8af0634b2
Akalyanandhakumar <167529056+Akalyanandhakumar@users.noreply.github.com>
2024-06-25 18:06:23 +0530

    counting of no.of.digits in a number

---
+++ countnumofdigits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class countnumofdigits{
+    public static void main(String[] args){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+       String a=Integer.toString(n);
+       System.out.print(a.length());
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//n=678908
+//a=6
